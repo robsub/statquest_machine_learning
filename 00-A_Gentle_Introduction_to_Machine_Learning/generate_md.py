475ac40894421c3b99dd3280cbf725d036f852c2
Rob <32256514+robsub@users.noreply.github.com>
2023-04-06 18:38:27 +0100

    Delete 00-A_Gentle_Introduction_to_Machine_Learning directory

--- 00-A_Gentle_Introduction_to_Machine_Learning/generate_md.py
+++
@@ -1,9 +0,0 @@
-import os
-
-directory = "/Users/rob/Learning/statquest/00_-_A_Gentle_Introduction_to_Machine_Learning"
-
-with open("images.md", "w") as f:
-    for filename in os.listdir(directory):
-        if filename.endswith(".png"):
-            f.write("![{}](./{})\n".format(filename, filename))
-
